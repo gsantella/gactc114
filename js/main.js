4ee7f1e9311c61b71f389bf3b044e896a8a8582f
gsantella <gsantella@southhills.edu>
2024-11-02 14:46:31 -0400

    add some ISS

--- js/main.js
+++ js/main.js
@@ -13,3 +13,18 @@ document.getElementById('btnConvert').addEventListener('click', () => {
     const cTemp = Number(fTemp) + 1
     document.getElementById('cTemp').value = cTemp
 })
+
+document.getElementById('btnISS').addEventListener('click', async () => {
+    const apiUrl = 'http://api.open-notify.org/iss-now.json'
+    
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    
+    // add row to table
+    const x = document.getElementById('tblISS');
+    let new_row = x.rows[1].cloneNode(true);
+    new_row.cells[0].innerText = json.iss_position.latitude;
+    new_row.cells[1].innerText = json.iss_position.longitude;
+    //console.log(new_row);
+    x.prepend( new_row );
+})
